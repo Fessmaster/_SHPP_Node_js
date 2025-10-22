8486bca9ff2eb525bad914cebf4e29c5a5c95781
Fessmaster <thefessmaster@gmail.com>
2025-10-22 19:46:25 +0300

    Added route in task 1.2.6

--- SHPP_level_1_2/1.2.6. express/app.js
+++ SHPP_level_1_2/1.2.6. express/app.js
@@ -14,6 +14,37 @@ try {
 }
 
 app.get("/", (req, res) => {
+  return res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View counter</title>
+</head>
+<body style="background-color: rgb(36, 36, 36);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; 
+            margin: 0;">
+
+    <div style="width: 400px;
+                height: 150px;
+                background-color: rgb(212, 212, 212);
+                display: flex;
+                justify-content: center;
+                align-items: center; 
+                border-radius: 10px;">
+
+        <h1><a href="/hello">Counter</a></h1>            
+        
+    </div>
+    
+</body>
+</html>`);
+})
+
+app.get("/hello", (req, res) => {
   counter = parseInt(counter) + 1;
   fs.writeFileSync(filePath, counter.toString());
   return res.send(`<!DOCTYPE html>
