18e86f92035a3007a8f1575212089669f514b88e
fessmaster <100466544+Fessmaster@users.noreply.github.com>
2025-11-30 00:39:11 +0200

    saving the task list to a file has been implemented

--- SHPP_level_2_4/src/app.ts
+++ SHPP_level_2_4/src/app.ts
@@ -4,87 +4,77 @@ import * as path from "path"
 
 const app = express();
 const port = 3005;
-const filePath = path.join(__dirname, "../public/listOfTasks.txt"); 
-let ID = 0;
-let listOfTasks:string = '';
-let list: Array<string> = []
+const filePath = path.join(__dirname, "../public/listOfTasks.json"); 
 
-try {
-  listOfTasks = fs.readFileSync(filePath).toString();  
-} catch (error: any) {
-  console.log(error.message);
+interface Task {
+  id: number,
+  text: string,
+  checked: boolean
 }
 
-function printList (list:string){
-  const arrayOfList = list.split('\n')
-  if (arrayOfList.length > 0){
-    const listOfTasks = arrayOfList.map(list => {
-      const listOfItems = list.split(',');
-      return {
-        id: listOfItems[0],
-        text: listOfItems[1],
-        checked: listOfItems[2]
-      }
-    })
-    listOfTasks.forEach(list => console.log(list))
+let listOfTasks: Array<Task> = [];
+
+function parseObject() {
+  try {
+    listOfTasks = JSON.parse(fs.readFileSync(filePath).toString())
+  } catch (error: any) {
+    console.log(error.message);
   }
 }
-printList(listOfTasks)
+function getId():number{
+  if (listOfTasks.length > 0){
+    return listOfTasks[listOfTasks.length-1].id
+  }
+  return 0
+}
+parseObject();
 
+function reloadList(){
+  fs.writeFileSync(filePath, JSON.stringify(listOfTasks));  
+}
 
-
-// fs.writeFileSync(filePath, counter.toString());
-
-
-let items =  [ 
-  { id: 22, text: "Do some thing", checked: true } , 
-  { id: 23, text: "Read a book", checked: false } , 
-  { id: 24, text: "Complete task", checked: false } , 
-]
 app.use(express.json());
 app.use(express.static('public'))
 
 app.get('/api/v1/items',  (req, res) => {
   res.json({
-    items: items
+    items: listOfTasks
   }) 
 })
 app.post('/api/v1/items', (req, res) => {
   const newTask = req.body.text;
-  items.push({id: ID++, text: newTask, checked:false})
-  const newTaskInFile = `${ID++},${newTask},${false}\n`
-  
-  fs.appendFileSync(filePath, newTaskInFile);
+  let ID = listOfTasks.length > 0 ? listOfTasks[listOfTasks.length-1].id : 0
+  listOfTasks.push({id: ++ID, text: newTask, checked:false})
+  reloadList()
   res.json({
-    id: ID++, text: newTask, checked:false
-  })
-  res.end()
+    id: ID, text: newTask, checked:false
+  })  
 })
 
 app.put('/api/v1/items', (req, res) =>{
-  const item = items.find(item => item.id === req.body.id);  
+  const item = listOfTasks.find(item => item.id === req.body.id);  
 
   if (item) {
     Object.assign(item, req.body)
+    reloadList()
     res.json({ok:true});
-    res.end();    
   } else {
     res.status(404).json({error: `Task not found`})
   }
 })
 
 app.delete('/api/v1/items', (req, res) =>{
-  const item = items.find(item => item.id === req.body.id);    
+  const item = listOfTasks.find(item => item.id === req.body.id);    
   
   if (item){
-    items = items.filter(confirmed => confirmed.id !== item.id)
+    listOfTasks = listOfTasks.filter(confirmed => confirmed.id !== item.id)
+    reloadList()
     res.json({ok:true});
-    res.end();
   } else {
     res.status(404).json({error: `Task not found`})
   }
-
 })
+
 app.listen(port, () =>{
   console.log(`Server was started on port ${port}`);
 })
